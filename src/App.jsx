59972f63b25fae345a229c5ee04f6f0b35f25d9a
shakib2123 <mshakibalhasan21@gmail.com>
2023-09-14 20:14:14 +0600

    basic structure created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Courses from "./components/Courses/Courses";
+
+function App() {
+  return (
+    <>
+      <Courses></Courses>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,14 @@
+const Courses = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1 className="text-4xl font-bold">Course Registration</h1>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Courses;
