df6a110afdb96322787b04ae6e52df20dcf6292c
shakib2123 <mshakibalhasan21@gmail.com>
2023-09-14 23:09:20 +0600

    card section created

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,5 +1,38 @@
-const Course = () => {
-  return <div></div>;
+import PropTypes from "prop-types";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoBook } from "react-icons/go";
+const Course = ({ course, handleSelection }) => {
+  const { thumbnail, course_title, course_detail, price, credit_time } = course;
+  return (
+    <div className="space-y-4  bg-white border-gray-400 p-3 rounded-lg shadow-lg">
+      <figure className="w-full">
+        <img className="w-full" src={thumbnail} alt="" />
+      </figure>
+      <h2 className="text-lg font-semibold">{course_title}</h2>
+      <p>{course_detail}</p>
+      <div className="flex justify-between">
+        <p className="flex gap-1 items-center">
+          <BsCurrencyDollar></BsCurrencyDollar>
+          <p className="font-medium">Price : {price}</p>
+        </p>
+        <p className="flex gap-1 items-center">
+          <GoBook></GoBook>
+          <p className="font-medium">Credit : {credit_time}hr</p>
+        </p>
+      </div>
+      <button
+        onClick={() => handleSelection(course)}
+        className="font-medium text-white bg-[#2F80ED]  w-full"
+      >
+        Select
+      </button>
+    </div>
+  );
+};
+
+Course.propTypes = {
+  course: PropTypes.object.isRequired,
+  handleSelection: PropTypes.func.isRequired,
 };
 
 export default Course;
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import Course from "../Course/Course";
+import Cart from "../Cart/Cart";
+
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
+  const [selectCourse, setSelectCourse] = useState([]);
+  useEffect(() => {
+    fetch("./course.json")
+      .then((response) => response.json())
+      .then((data) => setCourses(data));
+  }, []);
+  const handleSelection = (course) => {
+    const isExist = selectCourse.find((item) => item.id === course.id);
+    if (isExist) {
+      return alert("You already select this course.");
+    } else {
+      setSelectCourse([...selectCourse, course]);
+    }
+  };
   return (
     <div>
-      <div>
-        <div>
-          <h1 className="text-4xl font-bold">Course Registration</h1>
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-10">
+        Course Registration
+      </h1>
+      <div className="flex flex-col md:flex-row gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {courses.map((course) => (
+            <Course
+              key={course.id}
+              course={course}
+              handleSelection={handleSelection}
+            ></Course>
+          ))}
         </div>
-        <div></div>
+        <div className="w-2/3">
+          <Cart selectCourse={selectCourse}></Cart>
+        </div>
       </div>
     </div>
   );
